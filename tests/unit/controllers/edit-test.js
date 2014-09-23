4204e0ab81dd53887c0217b18110d2b07ebb8a06
bttf <adnanchowdhury88@gmail.com>
2014-09-22 22:24:28 -0400

    added controller for article/edit

--- tests/unit/controllers/edit-test.js
+++ tests/unit/controllers/edit-test.js
@@ -1,11 +1,12 @@
 import { test, moduleFor } from 'ember-qunit';
 
-moduleFor('route:create', 'CreateRoute', {
+moduleFor('controller:edit', 'EditController', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
 
+// Replace this with your real tests.
 test('it exists', function() {
-  var route = this.subject();
-  ok(route);
+  var controller = this.subject();
+  ok(controller);
 });
