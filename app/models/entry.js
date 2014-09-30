4bd4330a791ceae684e727ffb9f0431cfc0af599
bttf <adnanchowdhury88@gmail.com>
2014-09-30 17:05:40 -0400

    switching from article to entry; switching from es to mongo

---
+++ app/models/entry.js
@@ -0,0 +1,14 @@
+import DS from 'ember-data';
+
+var attr = DS.attr;
+
+export default DS.Model.extend({
+  title: attr('string'),
+  tags: attr(),
+  type: attr('string'),
+  body: attr('string'),
+  url: attr('string'),
+  created: attr('date'),
+  modified: attr('date')
+
+});
--- app/router.js
+++ app/router.js
@@ -2,12 +2,20 @@ import Ember from 'ember';
 
 var Router = Ember.Router.extend({
   location: FrontendENV.locationType
+
 });
 
 Router.map(function() {
+  this.resource('entry', { path: '/entry/:entry_id' });
+
   this.resource('article', { path: '/article/:article_id' }, function() {
     this.route('edit');
+
   });
+
+  this.route('entry');
+
 });
 
 export default Router;
+
