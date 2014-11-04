31c0528906dffd0880817f040301fbdf38a6bd01
bttf <adnanchowdhury88@gmail.com>
2014-11-04 15:15:03 -0500

    added save while working functionality to entry edit page

--- app/controllers/edit.js
+++ app/controllers/edit.js
@@ -2,6 +2,16 @@ import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
   actions: {
+    tempSave: function() {
+      if (typeof this.get('model.tags') === 'string')
+        this.set('model.tags', this.get('model.tags').split(','));
+      var controller = this;
+      this.get('model').save().then(function(res) {
+        controller.set('model.saved', true);
+        controller.set('model.lastSave', res._data.modified)
+      }, function(err) {
+      });
+    },
     save: function() {
       if (typeof this.get('model.tags') === 'string')
         this.set('model.tags', this.get('model.tags').split(','));
