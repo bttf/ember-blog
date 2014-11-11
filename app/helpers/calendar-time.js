ba9be15a110e748cff6d3b93a6991afea80a5a86
bttf <adnanchowdhury88@gmail.com>
2014-11-11 12:22:56 -0500

    moment.js added for readable timestamps

---
+++ app/helpers/calendar-time.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+
+function calendarTime(value) {
+  if (!value || value === "")
+    return value;
+
+  var time = moment(value).calendar();
+
+  return new Ember.Handlebars.SafeString(time);
+
+}
+
+export {
+ calendarTime 
+};
+
+export default Ember.Handlebars.makeBoundHelper(calendarTime);
