3e225baa4ba251d32abf802b76196edf3db598f9
bttf <adnanchowdhury88@gmail.com>
2015-02-17 11:03:24 -0500

    auto-save

--- app/components/entry-editor.js
+++ app/components/entry-editor.js
@@ -1,6 +1,24 @@
 import Ember from 'ember';
 
+var $ = Ember.$;
+
 export default Ember.Component.extend({
+  autoSaveEnabled: false,
+  autoSaveTimeouts: [],
+
+  autoSaveHandler: function() {
+    var enabled = this.get('autoSaveEnabled');
+
+    if (enabled) {
+      var _this = this;
+      $('.body-input').keyup(function(e) {
+        pushAutoSaveTimeout(_this);
+      });
+    } else {
+      $('.body-input').off('keyup');
+    }
+  }.observes('autoSaveEnabled'),
+
   actions: {
     saveOnly: function(entry) {
       entry.set('tags', parseTags(entry.get('tags')));
@@ -20,4 +38,22 @@ function parseTags(tags) {
   if (typeof tags === 'string') {
     return tags.split(',');
   }
+}
+
+function pushAutoSaveTimeout(component) {
+  var arr = component.get('autoSaveTimeouts');
+  var saveFn = (function(_this) {
+    return function() {
+      _this.send('saveOnly', _this.get('entry'));
+    };
+  })(component);
+
+  // pop existing
+  if (arr.length > 0) {
+    for (var i = 0; i < arr.length; i++) {
+      window.clearTimeout(arr.pop());
+    }
+  }
+  var timeoutId = window.setTimeout(saveFn, 2000);
+  arr.push(timeoutId);
 }
